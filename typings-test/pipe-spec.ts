79c11ecac70f0f993955b8e858602e2a74c2394e
Deyan Totev <deyan.totev.extern@seven.one>
2022-11-19 14:22:12 +0200

    feat@typings test

---
+++ typings-test/pipe-spec.ts
@@ -0,0 +1,12 @@
+import {pipe} from 'roza'
+import * as R from 'rambda'
+
+describe('R.pipe', () => {
+  it('happy', () => {
+    const result = pipe(
+      1,
+      R.add(1)
+    )
+    result // $ExpectType number
+  })
+})
